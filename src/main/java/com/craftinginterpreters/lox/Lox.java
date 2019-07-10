5d4f429f6b11357244836b1e4a77d951cac74925
zhengjunhao <changchunjunhao@gmail.com>
2019-07-10 23:48:51 +0800

    Add Parser

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -43,13 +43,25 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        for (Token token : tokens) {
-            System.out.println(token.toString());
+
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+        if (hadError) {
+            return;
         }
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
     static boolean hadError = false;
     private static void report(int line, String where, String message) {
