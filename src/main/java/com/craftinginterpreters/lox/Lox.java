5ed4574584e8bdb9306cf1825f101279d51a41d7
zhengjunhao <changchunjunhao@gmail.com>
2019-07-11 22:31:37 +0800

    Add the error-check in Interpreter

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public class Lox {
 
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -26,6 +28,9 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) {
             System.exit(65);
+        }
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -64,9 +69,15 @@ public class Lox {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
     }
-    static boolean hadError = false;
+
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+            "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
