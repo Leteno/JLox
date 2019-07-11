dd87503350ff76814ca9ea51808c2832675cc32b
zhengjunhao <changchunjunhao@gmail.com>
2019-07-11 21:49:30 +0800

    Add Interpreter

---
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,90 @@
+package com.craftinginterpreters.lox;
+
+public class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitTernaryExpr(Expr.Ternary expr) {
+        if (isTruthy(evaluate(expr.compare))) {
+            return evaluate(expr.valueOnTrue);
+        } else {
+            return evaluate(expr.valueOnFalse);
+        }
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                return (double)left <= (double)right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+            case SLASH:
+                return (double)left - (double)right;
+            case STAR:
+                return (double)left * (double)right;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+            default:
+                return null;
+        }
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object instanceof Boolean) {
+            return (boolean) object;
+        }
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        return a == null && b == null ||
+                a != null && a.equals(b);
+    }
+}
