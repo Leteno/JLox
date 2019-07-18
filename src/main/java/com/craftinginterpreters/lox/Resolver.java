e8cd90590ce6cf9ba20e64fa5611125dfe30bf02
zhengjunhao <changchunjunhao@gmail.com>
2019-07-18 23:40:08 +0800

    support 'this' expression

--- src/main/java/com/craftinginterpreters/lox/Resolver.java
+++ src/main/java/com/craftinginterpreters/lox/Resolver.java
@@ -79,6 +79,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitThisExpr(Expr.This expr) {
+        resolveLocal(expr, expr.keyword);
+        return null;
+    }
+
+    @Override
     public Void visitUnaryExpr(Expr.Unary expr) {
         resolve(expr.right);
         return null;
@@ -116,10 +122,15 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         declare(stmt.name);
         define(stmt.name);
 
+        beginScope();
+        scopes.peek().put("this", true);
+
         for (Stmt.Function method : stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
             resolveFunction(method, declaration);
         }
+
+        endScope();
 
         return null;
     }
